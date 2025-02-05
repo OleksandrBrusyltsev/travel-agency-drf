d92e237ea9f5034286dc39f2f1eb19a498062c72
Oleksandr <Brusiltsev1@gmail.com>
2025-02-05 17:22:53 +0200

    style: fix linter issues and improve code formatting

--- travel_agency/urls.py
+++ travel_agency/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # path('travel_agency/', include('travel_agency.urls')),  # Include the travel_agency app's URLs'
-
 ]
